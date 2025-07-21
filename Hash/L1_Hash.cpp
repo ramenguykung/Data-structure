2a609b9b1692edf6469d39f3a5b524bc3896625b
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-07-21 08:10:21 +0000

    [ongoing] Hash table content update

--- Hash/L1_Hash.cpp
+++ Hash/L1_Hash.cpp
@@ -25,12 +25,33 @@ class separateChaining {
 
 int main() {
     char cmd;
+    int key;
+    string value;
 
     cout << "Enter command: ";
     cin >> cmd;
+    
+    /**
+     * @brief Process the command input by the user.
+     * @param cmd The command character input by the user.
+     * @param key The key to be used in the hash table.
+     * @param value The value to be associated with the key in the hash table.
+     * @param a Add a new key-value pair to the hash table.
+     * @param p Print key-value pairs in the hash table.
+     * @param s Search for value by the given key.
+     * @param e Exit the program.
+     */
     switch (cmd) {
         case 'a':
-            
+            cin >> key >> value;
+        case 'p':
+        case 's':
+        case 'e':
+            exit(0);
+        default:
+            cout << "Invalid command!" << endl;
+            return 1;
+
     }
     return 0;
 }

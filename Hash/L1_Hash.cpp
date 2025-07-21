a6c7c16e8b2aa4012a091482c86d4e522b79567b
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-07-21 14:53:51 +0700

    [ongoing] Hash Table content update

--- Hash/L1_Hash.cpp
+++ Hash/L1_Hash.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+using namespace std;
+
+const int TABLE_SIZE = 17;
+/**
+ * @brief Hash table using separate chaining.
+ * This class implements a hash table using separate chaining
+ */
+class separateChaining {
+    public:
+        int hash_key[TABLE_SIZE];
+        string hash_table[TABLE_SIZE];
+        int n = 0;
+        int r = 0;
+
+        separateChaining(int p_n, int p_r) {
+            n = p_n;
+            r = p_r;
+            for(int i = 0; i < n; i++) {
+                hash_key[i] = -1;
+                hash_table[i] = "-";
+            }
+        }
+};
 
 int main() {
+    char cmd;
+
+    cout << "Enter command: ";
+    cin >> cmd;
+    switch (cmd) {
+        case 'a':
+            
+    }
     return 0;
 }

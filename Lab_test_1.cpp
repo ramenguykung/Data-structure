fb9181376c7fdebeada1572389f14ef61dac57df
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-08-04 14:12:19 +0700

    [ongoing] Lab content

---
+++ Lab_test_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class PID{
+    public:
+    int pid, arrivalTime, serviceTime;
+
+    PID() {
+        this->pid = 0;
+    }
+
+};
+
+int main() {
+    return 0;
+}
---
+++ Lab_test_2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int bracketMatch(const string& text) {
+    stack<char> st;
+    bool hasBrackets = false;
+    int matchCount = 0;
+
+    for (char c : text) {
+        if (c == '(' || c == '{' || c == '[' || c == '<') {
+            st.push(c);
+            hasBrackets = true;
+        } else if (c == ')' || c == '}' || c == ']' || c == '>') {
+            hasBrackets = true;
+            if (st.empty()) {
+                return 0;
+            }
+
+            char top = st.top();
+            st.pop();
+
+            if ((c == ')' && top != '(') || 
+                (c == '}' && top != '{') || 
+                (c == ']' && top != '[') ||
+                (c == '>' && top != '<')) {
+                return 0;
+            } else {
+                matchCount++;
+            }
+        }
+    }
+    return (st.empty() && hasBrackets) ? matchCount : 0;
+}
+
+int main() {
+    string input;
+
+    getline(cin, input);
+    if (bracketMatch(input)) {
+        cout << "Matched" << endl;
+        cout << bracketMatch(input) << " mathced" << endl;
+    } else {
+        cout << "Not Matched" << endl;
+        cout << bracketMatch(input) << " matched" << endl;
+    }
+    return 0;
+}

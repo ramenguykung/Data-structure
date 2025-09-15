4465854cda80c4e6ff8398a4cb6df4b20126e221
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2025-09-15 08:54:22 +0000

    [ongoing] Refactor AVL tree implementation and update command handling in main function

--- AVL/AVL.cpp
+++ AVL/AVL.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+using namespace std;
 
 class Node {
 	public:
@@ -7,12 +8,11 @@ class Node {
 		Node * left;
 		Node * right;
 		
-		Node () { // default constructor
+		Node(){ // default constructor
 			this->left = nullptr;
 			this->right = nullptr;
 		}
-		
-		Node (int data) {
+		Node(int data) {
 			this->data = data;
 			this->left = nullptr;
 			this->right = nullptr;
@@ -22,40 +22,124 @@ class Node {
 class tree {
 	public:
 		Node * h;
-		std::queue <Node*> q;
 		tree() {
-			h = new Node();
+			h = nullptr;
 		}
-		//Methods
-		void push(int data);
-		void popThis(int data);
-		void peekPreOrder();
+		// Methods
+		void push(int x) {
+			if (h == nullptr) {
+				h = new Node(x);
+				return;
+			}
+			Node* current = h;
+			while(true) {
+				if (x < current->data) {
+					if (current->left == nullptr) {
+						current->left = new Node(x);
+						return;
+					}
+					current = current->left;
+				} else if (x > current->data) {
+					if (current->right == nullptr) {
+						current->right = new Node(x);
+						return;
+					}
+					current = current->right;
+				} else {
+					return;
+				}
+			}
+		}
+		void pop(int x) {
+			if (h == nullptr) return;  // Empty tree
+
+			Node* current = h;
+			Node* parent = nullptr;
+
+			// Find the node and its parent
+			while (current != nullptr && current->data != x) {
+				parent = current; 
+				if (x < current->data) {
+					current = current->left;
+				} else {
+					current = current->right;
+				}
+			}
+			if (current == nullptr) return;  // Not found
+
+			// Now delete current
+			if (current->left == nullptr && current->right == nullptr) {
+				// Leaf
+				if (parent == nullptr) h = nullptr;
+				else if (parent->left == current) parent->left = nullptr;
+				else parent->right = nullptr;
+				delete current;
+			} else if (current->left == nullptr) {
+				// One right child
+				if (parent == nullptr) h = current->right;
+				else if (parent->left == current) parent->left = current->right;
+				else parent->right = current->right;
+				delete current;
+			} else if (current->right == nullptr) {
+				// One left child
+				if (parent == nullptr) h = current->left;
+				else if (parent->left == current) parent->left = current->left;
+				else parent->right = current->left;
+				delete current;
+			} else {
+				// Two children: Find in-order successor
+				Node* successor = current->right;
+				Node* succParent = current;
+				while (successor->left != nullptr) {
+					succParent = successor;
+					successor = successor->left;
+				}
+				// Swap data
+				current->data = successor->data;
+				// Delete successor (which is now a leaf or has right child)
+				if (succParent->left == successor) succParent->left = successor->right;
+				else succParent->right = successor->right;
+				delete successor;
+			}
+		}
+		void peekPreOrder() {
+			preorder(h);
+			cout << endl;
+		}
+		void preorder(Node * node) {
+			if (node == nullptr) return;
+
+			cout << node->data << ",";
+			preorder(node->left);
+			preorder(node->right);
+		}
 };
 
-void tree::push(int data) {
-}
-void tree::popThis(int data) {
-}
-void tree::peekPreOrder() {
-}
 int main() {
 	tree * t = new tree();
 	char command;
 	int data;
-	
-	while(std::cin >> command) {
-		if (command == 'a') {
+	cin >> command;
+	while(true) {
+		switch(command) {
+			case 'a':
+				cin >> data;
+				t->push(data);
+				break;
+			case 'd':
+				cin >> data;
+				t->pop(data);
+				break;
+			case 'p':
+				t->peekPreOrder();
+				break;
+			
+			case 'x':
+				exit(0);
+			default:
+				cout << "Invalid command, try again!";
+				break;
 		}
-		else if (command == 'd') {
-		}
-		else if (command == 'p') {
-		}
-		else if (command == 'x' || command == 'X'){
-			break;
-		}
-		else {
-			std::cout << "Invalid command, try again!" << std::endl;
-		}
+		cin >> command;
 	}
-	return 0;
 }
